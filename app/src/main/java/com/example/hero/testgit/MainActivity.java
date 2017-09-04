252f77fd502a80d8985e6b08a2aa16f084d58ffe
丁远帅 <2441547057@qq.com>
2017-09-04 16:06:38 +0800

    add 1.1

--- app/src/main/java/com/example/hero/testgit/MainActivity.java
+++ app/src/main/java/com/example/hero/testgit/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    private String str="hello test v1.0";
+    private String str="hello test v1.1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
